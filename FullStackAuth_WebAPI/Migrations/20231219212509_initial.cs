5c219a0c2696d16650f322313270c48826e2133b
Robert Kemp <robert.kemp04@outlook.com>
2023-12-19 15:29:54 -0600

    starter code

---
+++ FullStackAuth_WebAPI/Migrations/20231219212509_initial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FullStackAuth_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "02826bcd-2b15-4c0a-8d85-281ade12b9b9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "59de2413-2986-49fa-a7ea-d2ee9bae8830");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "63ed2358-c3db-4bf7-9c68-7a5daf9ce09d", null, "Admin", "ADMIN" },
+                    { "b4f5cd14-b249-4f6b-8cc1-333e1a2883ab", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "63ed2358-c3db-4bf7-9c68-7a5daf9ce09d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4f5cd14-b249-4f6b-8cc1-333e1a2883ab");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "02826bcd-2b15-4c0a-8d85-281ade12b9b9", null, "Admin", "ADMIN" },
+                    { "59de2413-2986-49fa-a7ea-d2ee9bae8830", null, "User", "USER" }
+                });
+        }
+    }
+}
